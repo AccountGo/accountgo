85fbc2016bf70142844527754acf03d61cac3a94
codemvp <codemvp@gmail.com>
2015-08-03 10:03:01 +0800

    CREATE DATABASE permission denied in database 'master'.

--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -48,10 +48,13 @@ namespace Web
             //dbcontext
             if (HttpContext.Current.Request.IsLocal)
             {
-                var context = new ApplicationContext(ConfigurationManager.ConnectionStrings["ApplicationContext"].ConnectionString);
-                var dbInitializer = new DbInitializer<ApplicationContext>();
-                Database.SetInitializer<ApplicationContext>(dbInitializer);
-                dbInitializer.InitializeDatabase(context);
+                //var context = new ApplicationContext(ConfigurationManager.ConnectionStrings["ApplicationContext"].ConnectionString);
+                //var dbInitializer = new DbInitializer<ApplicationContext>();
+                //Database.SetInitializer<ApplicationContext>(dbInitializer);
+                //dbInitializer.InitializeDatabase(context);
+                //builder.Register<IDbContext>(c => context).SingleInstance();
+                Database.SetInitializer<ApplicationContext>(null);
+                var context = new ApplicationContext("ApplicationContext");
                 builder.Register<IDbContext>(c => context).SingleInstance();
             }
             else
