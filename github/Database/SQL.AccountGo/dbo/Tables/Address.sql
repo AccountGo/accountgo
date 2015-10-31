b36bb62eab2bdc6125d23abb19d50cca5bad26d6
codemvp <mvpsolution@gmail.com>
2015-10-31 17:44:36 +0800

    folder restructure

--- github/Database/SQL.AccountGo/dbo/Tables/Address.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Address] (
-    [Id]         INT            IDENTITY (1, 1) NOT NULL,
-    [CreatedBy]  NVARCHAR (MAX) NULL,
-    [CreatedOn]  DATETIME       NOT NULL,
-    [ModifiedBy] NVARCHAR (MAX) NULL,
-    [ModifiedOn] DATETIME       NOT NULL,
-    CONSTRAINT [PK_dbo.Address] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
