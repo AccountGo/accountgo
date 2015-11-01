d0d3873cd4093e547fe8f36cadff37c78e1e4cbb
codemvp <mvpsolution@gmail.com>
2015-11-01 12:39:00 +0800

    add unit testing project

---
+++ Test/UnitTests/TaxComputationTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TaxComputationTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
