d844b306518bc1610f77340554ff5aba4d61d63a
Marvin Perez <marvinperez@outlook.com>
2016-03-03 23:33:35 +0800

    implement adfs authentication on webangular project

--- src/WebAngular/Startup.cs
+++ src/WebAngular/Startup.cs
@@ -1,32 +1,104 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNet.Authentication.Cookies;
+using Microsoft.AspNet.Authentication.OpenIdConnect;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.IdentityModel.Tokens;
+using System.Threading.Tasks;
 
 namespace WebAngular
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            // Set up configuration sources.
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+        }
+        public IConfigurationRoot Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add framework services.
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
             // Add the platform handler to the request pipeline.
             app.UseIISPlatformHandler();
 
             app.UseDefaultFiles();
-            app.UseStaticFiles();
-        }
 
+            app.UseStaticFiles();
+
+            app.UseCookieAuthentication(options =>
+            {
+                options.AutomaticAuthenticate = true;
+            });
+
+            app.UseOpenIdConnectAuthentication(options =>
+            {
+                options.AutomaticChallenge = true;
+                options.ClientId = Configuration["Authentication:AzureAd:ClientId"];
+                options.Authority = Configuration["Authentication:AzureAd:AADInstance"] + "Common";
+                options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    // Instead of using the default validation (validating against a single issuer value, as we do in line of business apps),
+                    // we inject our own multitenant validation logic
+                    ValidateIssuer = false,
+
+                    // If the app needs access to the entire organization, then add the logic
+                    // of validating the Issuer here.
+                    // IssuerValidator
+                };
+                options.Events = new OpenIdConnectEvents()
+                {
+                    OnAuthenticationValidated = (context) =>
+                    {
+                        // If your authentication logic is based on users then add your logic here
+                        return Task.FromResult(0);
+                    },
+                    OnAuthenticationFailed = (context) =>
+                    {
+                        context.Response.Redirect("/Home/Error");
+                        context.HandleResponse(); // Suppress the exception
+                        return Task.FromResult(0);
+                    }
+                };
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    
         // Entry point for the application.
         public static void Main(string[] args) => WebApplication.Run<Startup>(args);
     }
--- src/WebAngular/gulpfile.js
+++ src/WebAngular/gulpfile.js
@@ -1,11 +1,16 @@
-﻿/// <binding AfterBuild='lib, rxjs' Clean='clean' />
+﻿/// <binding Clean='clean' />
+"use strict";
 
-var gulp = require('gulp');
-var rimraf = require('rimraf');
+var gulp = require("gulp");
+var rimraf = require("rimraf");
+var concat = require("gulp-concat");
+//var cssmin = require("gulp-cssmin");
+//var uglify = require("gulp-uglify");
 
 var paths = {
     npm: './node_modules/',
-    lib: './wwwroot/lib/'
+    angular2: './wwwroot/lib/angular2/',
+    webroot: './wwwroot/'
 };
 
 var libs = [
@@ -17,14 +22,47 @@ var libs = [
     paths.npm + 'systemjs/dist/system-polyfills.js'
 ];
 
+paths.js = paths.webroot + "js/**/*.js";
+paths.minJs = paths.webroot + "js/**/*.min.js";
+paths.css = paths.webroot + "css/**/*.css";
+paths.minCss = paths.webroot + "css/**/*.min.css";
+paths.concatJsDest = paths.webroot + "js/site.min.js";
+paths.concatCssDest = paths.webroot + "css/site.min.css";
+
+gulp.task("clean:angular2", function (callback) {
+    rimraf(paths.angular2, callback);
+});
+
+gulp.task("clean:js", function (cb) {
+    rimraf(paths.concatJsDest, cb);
+});
+
+gulp.task("clean:css", function (cb) {
+    rimraf(paths.concatCssDest, cb);
+});
+
+gulp.task("clean", ["clean:angular2", "clean:js", "clean:css"]);
+
+gulp.task("min:js", function () {
+    return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
+        .pipe(concat(paths.concatJsDest))
+        //.pipe(uglify())
+        .pipe(gulp.dest("."));
+});
+
+gulp.task("min:css", function () {
+    return gulp.src([paths.css, "!" + paths.minCss])
+        .pipe(concat(paths.concatCssDest))
+        //.pipe(cssmin())
+        .pipe(gulp.dest("."));
+});
+
+gulp.task("min", ["min:js", "min:css"]);
+
 gulp.task('rxjs', function () {
-    return gulp.src(paths.npm + 'rxjs/**/*.js').pipe(gulp.dest(paths.lib + 'rxjs/'));
+    return gulp.src(paths.npm + 'rxjs/**/*.js').pipe(gulp.dest(paths.angular2 + 'rxjs/'));
 });
 
-gulp.task('lib', ['rxjs'], function () {
-    return gulp.src(libs).pipe(gulp.dest(paths.lib));
-});
-
-gulp.task('clean', function (callback) {
-    rimraf(paths.lib, callback);
+gulp.task('angular2', ['rxjs'], function () {
+    return gulp.src(libs).pipe(gulp.dest(paths.angular2));
 });
