ab2a46bf7fb4874251b5b80f1857bf8a2c98553f
Marvin Perez <marvinvperez@coherentasia.com>
2016-06-12 19:41:39 +0800

    arrange webpack config

--- src/AccountGoWeb/webpack.config.js
+++ src/AccountGoWeb/webpack.config.js
@@ -2,12 +2,13 @@
 var path = require('path');
 
 var buildDir = path.resolve(__dirname, './wwwroot/scripts');
-var scriptsDir = path.resolve(__dirname, './wwwroot/libs/tsxout');
+var scriptsDir = path.resolve(__dirname, './wwwroot/libs/tsxbuild');
 
 var config = {
     entry: {
         home: scriptsDir + '/home' + '/home.js',
-        addSalesOrder: scriptsDir + '/sales/addsalesorder.js'
+        addSalesOrder: scriptsDir + '/sales/addsalesorder.js',
+        vendor: ['react', 'react-dom']
     },
     output: {
         path: buildDir,
@@ -15,15 +16,18 @@ var config = {
     },
     debug: true,
     devtool: 'source-map',
-    module: {
-        loaders: [
-          {
-              test: /\.tsx$/,
-              loader: 'ts-loader',
-              exclude: /(node_modules)/
-          }
-        ]
-    }
+    //module: {
+    //    loaders: [
+    //      {
+    //          test: /\.tsx$/,
+    //          loader: 'ts-loader',
+    //          exclude: /(node_modules)/
+    //      }
+    //    ]
+    //},
+    plugins: [
+    new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.bundle.js")
+    ],
 };
 
 module.exports = config;
