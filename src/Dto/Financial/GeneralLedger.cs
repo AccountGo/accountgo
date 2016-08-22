175c2adea7009f5e48f2aae516fae61942c08e6b
goiwebdev <goiwebdev@gmail.com>
2016-08-22 18:37:46 +0800

    General ledger task. (#53)

--- src/Dto/Financial/GeneralLedger.cs
+++ src/Dto/Financial/GeneralLedger.cs
@@ -1,6 +1,27 @@
-﻿namespace Dto.Financial
+﻿using System.Collections.Generic;
+
+namespace Dto.Financial
 {
     public class GeneralLedger : BaseDto
     {
+
+        public int GeneralLedgerHeaderId { get; set; }
+        public int AccountId { get; set; }
+        public DrOrCrSide DrCr { get; set; }
+        public decimal Amount { get; set; }
+
+
+        public IList<GeneralLedger> ChildGeneralLedger { get; set; }
+
+        public GeneralLedger()
+        {
+            ChildGeneralLedger = new List<GeneralLedger>();
+        }
+        public enum DrOrCrSide
+        {
+            NA = 0,
+            Dr = 1,
+            Cr = 2
+        }
     }
 }
