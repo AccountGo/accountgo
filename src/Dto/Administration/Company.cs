b819a1f74d0802a8feddccfcaf7f656e3abc8aea
SohailQureshi <sohail.qureshi@property-want.com>
2016-10-06 16:14:39 +0100

    1. Data Annotations added to Company Dto 2. Css & JQuery logic updated in Company.cshtml

--- src/Dto/Administration/Company.cs
+++ src/Dto/Administration/Company.cs
@@ -1,12 +1,22 @@
-﻿namespace Dto.Administration
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dto.Administration
 {
-    public class Company : BaseDto
-    {
-        [System.ComponentModel.DataAnnotations.Required]
-        public string Name { get; set; }
-        [System.ComponentModel.DataAnnotations.Required]
-        public string ShortName { get; set; }
-        [System.ComponentModel.DataAnnotations.Required]
-        public string CompanyCode { get; set; }
-    }
+  public class Company : BaseDto
+  {
+    [Required]
+    [Display(Name = "Company Code")]
+    public string CompanyCode { get; set; }
+
+    [Required]
+    [Display(Name = "Name")]
+    public string Name { get; set; }
+
+    [Required]
+    [Display(Name = "Short Name")]
+    public string ShortName { get; set; }
+
+    [Display(Name = "Logo")]
+    public byte[] Logo { get; set; }
+  }
 }
