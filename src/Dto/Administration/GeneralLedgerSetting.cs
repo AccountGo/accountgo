8b64154a1f352d5f87cee8b6fa508938013a3f2a
SohailQureshi <sohail.qureshi@property-want.com>
2016-10-06 16:30:31 +0100

    1. General Ledger Settings Dto converted 2. Settings.cshtml revamped with Css and JQuery 3. AdministrationController revamped to work with settings

---
+++ src/Dto/Administration/GeneralLedgerSetting.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dto.Administration
+{
+  public class GeneralLedgerSetting : BaseDto
+  {
+    [Display(Name = "Company")]
+    public int? CompanyId { get; set; }
+    [Display(Name = "Payable Account")]
+    public int? PayableAccountId { get; set; }
+    [Display(Name = "Purchase Discount Account")]
+    public int? PurchaseDiscountAccountId { get; set; }
+    [Display(Name = "Goods Receipt Note Clearing Account")]
+    public int? GoodsReceiptNoteClearingAccountId { get; set; }
+    [Display(Name = "Sales Discount Account")]
+    public int? SalesDiscountAccountId { get; set; }
+    [Display(Name = "Shipping Charge Account")]
+    public int? ShippingChargeAccountId { get; set; }
+    [Display(Name = "Permanent Account")]
+    public int? PermanentAccountId { get; set; }
+    [Display(Name = "Income Summary Account")]
+    public int? IncomeSummaryAccountId { get; set; }
+  }
+}
