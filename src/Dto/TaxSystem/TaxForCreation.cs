5692d6fdac8535d7a7fe39b96508e824db65b205
ydongchoi <105weapon@gmail.com>
2024-07-31 13:49:49 -0700

    [feature] Created TaxForCreation Dto for between mvc app and api server.

---
+++ src/Dto/TaxSystem/TaxForCreation.cs
@@ -0,0 +1,16 @@
+﻿namespace Dto.TaxSystem
+{
+    public class TaxForCreation
+    {
+        public int SalesAccountId { get; set; }
+        public int PurchaseAccountId { get; set; }
+
+        public string TaxName { get; set; }
+        public string TaxCode { get; set; }
+        public decimal Rate { get; set; }
+        public bool IsActive { get; set; }
+
+        public int TaxGroupId { get; set; }
+        public int ItemTaxGroupId { get; set; }
+    }
+}
