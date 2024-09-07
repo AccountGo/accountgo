49b4147d2b0685ec84597bcd50fc25fade8e59b7
ydongchoi <105weapon@gmail.com>
2024-09-07 11:04:42 -0700

    [feature] Added Null Error Code in Error.

--- src/Core/Domain/Error/Error.cs
+++ src/Core/Domain/Error/Error.cs
@@ -4,9 +4,11 @@
     {
         private static readonly string RecordNotFoundCode = "RecordNotFound";
         private static readonly string ValidationErrorCode = "ValidationError";
+        private static readonly string NullErrorCode = "NullError";
 
         public static readonly Error None = new(string.Empty, string.Empty);
         public static Error RecordNotFound(string message) => new Error(RecordNotFoundCode, message);
         public static Error ValidationError(string message) => new Error(ValidationErrorCode, message);
+        public static Error NullError(string message) => new Error(NullErrorCode, message);
     }
 }
