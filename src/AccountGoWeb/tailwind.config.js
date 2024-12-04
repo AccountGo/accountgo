428e7e36a08fc1c4590ee86af5153fc5a9348e1d
Fiona Wong <fwwong01@gmail.com>
2024-12-03 21:20:59 -0800

    Updated custom tailwind file

--- src/AccountGoWeb/tailwind.config.js
+++ src/AccountGoWeb/tailwind.config.js
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
-		"./Pages/**/*.cshtml",
-		"./Views/**/*.cshtml",
-		"./wwwroot/**/*.html",
+	  "./Pages/**/*.cshtml",
+	  "./Views/**/*.cshtml",
+	  "./wwwroot/**/*.html",
 	],
 	theme: {
-		extend: {
-			colors: {
-				darkPrimary: { DEFAULT: "#0B2545" },
-				darkSecondary: { DEFAULT: "#134074" },
-				lightPrimary: { DEFAULT: "#8DA9C4" },
-				lightSecondary: { DEFAULT: "#eef4ed" },
+	  extend: {
+		colors: {
+		  darkPrimary: { DEFAULT: "#0B2545" },
+		  darkSecondary: { DEFAULT: "#134074" },
+		  lightPrimary: { DEFAULT: "#8DA9C4" },
+		  lightSecondary: { DEFAULT: "#eef4ed" },
 		},
+	  },
 	},
 	plugins: [],
-}
-}
+  }
+  
