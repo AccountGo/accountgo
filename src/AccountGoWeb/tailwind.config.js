c9957162a383e663cadef1a858f81712fa95ef86
GLOBALYZ <GlobalYZ@139.com>
2024-12-04 03:28:33 -0800

    complete tables

--- src/AccountGoWeb/tailwind.config.js
+++ src/AccountGoWeb/tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-	  "./Pages/**/*.cshtml",
-	  "./Views/**/*.cshtml",
-	  "./wwwroot/**/*.html",
-	],
-	theme: {
-	  extend: {
-		colors: {
-		  darkPrimary: { DEFAULT: "#0B2545" },
-		  darkSecondary: { DEFAULT: "#134074" },
-		  lightPrimary: { DEFAULT: "#8DA9C4" },
-		  lightSecondary: { DEFAULT: "#eef4ed" },
-		},
-	  },
-	},
-	plugins: [],
-  }
-  
+  content: [
+    "./Pages/**/*.{cshtml,razor}",
+    "./Views/**/*.{cshtml,razor}",
+    "./Components/**/*.{cshtml,razor}",
+    "./wwwroot/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        darkPrimary: { DEFAULT: "#0B2545" },
+        darkSecondary: { DEFAULT: "#134074" },
+        lightPrimary: { DEFAULT: "#8DA9C4" },
+        lightSecondary: { DEFAULT: "#eef4ed" },
+      },
+    },
+  },
+  plugins: [],
+};
