d736cdf3e27b53941a1310103f39253073e396c9
Alfrey-Chan <122188063+Alfrey-Chan@users.noreply.github.com>
2024-12-03 16:42:52 -0800

    Integrate TailWindCss

---
+++ src/AccountGoWeb/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./Pages/**/*.cshtml",
+		"./Views/**/*.cshtml",
+		"./wwwroot/**/*.html",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
